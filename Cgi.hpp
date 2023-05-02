f5975e6e1039c4f932349d5bf84960508ea3b00d
Y2IN <dpdls0228@daum.net>
2023-05-02 15:10:27 +0900

    feat : cgi 환경변수 세팅2

--- Cgi.hpp
+++ Cgi.hpp
@@ -6,27 +6,27 @@
 /*   By: yje <yje@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:35:20 by yje               #+#    #+#             */
-/*   Updated: 2023/04/28 18:23:44 by yje              ###   ########.fr       */
+/*   Updated: 2023/05/01 17:31:57 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 #define CGI_HPP
 
-class CGI {
-	private:
-		cgi(void);
-	public:
-		CGI();
-		CGI(const CGI &src);
-		CGI &operator=(const CGI &src);
-		~CGI(void);
-		cgiRequest();
-		initEnvp();
-		getenv(const char* env);
+#include <map>
+class CGI
+{
+	// private:
+	//
+public:
+	CGI();
+	CGI(const CGI &src);
+	CGI &operator=(const CGI &src);
+	~CGI(void);
+	// cgiRequest();
+	void initEnvp();
+	// getenv(const char *env);
 
-		std::map<std::string, std::string> _envp;
-
-
+	std::map<std::string, std::string> envp_;
 };
 #endif;
--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <cstdlib> // for getenv
 
-int main() {
+int main()
+{
     // Content-type header
     std::cout << "Content-type:text/html\r\n\r\n";
 
     // Query string
-    char* query_string = getenv("QUERY_STRING");
-    std::cout << "<p>Query string: " << query_string << "</p>";
+    char *query_string = getenv("QUERY_STRING");
+    if (query_string != nullptr)
+    {
+        std::cout << "<p>Query string: " << query_string << "</p>";
+    }
+    // std::cout << "<p>Query string: " << query_string << "</p>";
 
     // HTTP_USER_AGENT environment variable
-    char* user_agent = getenv("HTTP_USER_AGENT");
-    std::cout << "<p>User agent: " << user_agent << "</p>";
+    std::string input;
+    if (std::getline(std::cin, input))
+    {
+        std::cout << "<p>Input: " << input << "</p>";
+    }
+    char *user_agent = getenv("HTTP_USER_AGENT");
+    // std::cout << "<p>User agent: " << user_agent << "</p>";
 
     // Form data
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << "<p>Input: " << input << "</p>";
+    // std::string input;
+    // std::getline(std::cin, input);
+    // std::cout << "<p>Input: " << input << "</p>";
 
     return 0;
 }
