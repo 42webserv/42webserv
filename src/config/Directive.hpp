960be4be8f6425140d29fa8b25902bf27252bc84
get6 <ittae.com@gmail.com>
2023-05-21 19:50:36 +0900

    feat: Directive를 쓰는 함수 추가  #19

--- src/config/Directive.hpp
+++ src/config/Directive.hpp
@@ -6,13 +6,14 @@
 /*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 20:15:35 by sunhwang          #+#    #+#             */
-/*   Updated: 2023/05/19 22:03:25 by sunhwang         ###   ########.fr       */
+/*   Updated: 2023/05/21 19:47:49 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DIRECTIVE_HPP
 #define DIRECTIVE_HPP
 
+#include <map>
 #include <string>
 #include <vector>
 
@@ -24,6 +25,8 @@ struct Directive
     std::vector<Directive> block;
 };
 
-Directive newDir(const std::string name, const std::string value);
+Directive newDir(const std::string name, const std::string value, const std::string pre_name);
+bool hasDirInBlock(Directive &dir, const std::string name);
+void setDirectivesToMap(std::map<std::string, std::string> &dirs, const std::pair<std::string, std::string> *ps, const size_t count);
 
 #endif
