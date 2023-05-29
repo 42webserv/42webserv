8b88a64727dadac5dd3382876990f0e66046b87e
get6 <ittae.com@gmail.com>
2023-05-29 22:21:26 +0900

    feat: I/O file 함수 추가 #109

--- src/process/commonProcess.cpp
+++ src/process/commonProcess.cpp
@@ -6,10 +6,11 @@
 /*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:20:15 by sunhwang          #+#    #+#             */
-/*   Updated: 2023/05/29 20:02:00 by sunhwang         ###   ########.fr       */
+/*   Updated: 2023/05/29 22:05:05 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <algorithm>
 #include "commonProcess.hpp"
 
 /**
@@ -57,4 +58,26 @@ bool isMethod(const std::string &method)
 		if (method == methods[i])
 			return true;
 	return false;
+}
+
+bool writeFile(const std::string &path, const std::string &contents)
+{
+	std::ofstream file(path.c_str(), std::ios::out | std::ios::trunc);
+
+	if (!file.is_open())
+		return false;
+	file << contents;
+	file.close();
+	return true;
+}
+
+std::string readFile(const std::string &path)
+{
+	std::ifstream file(path.c_str());
+
+	if (!file.is_open())
+		return "";
+	std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	file.close();
+	return contents;
 }
--- src/process/commonProcess.hpp
+++ src/process/commonProcess.hpp
@@ -6,7 +6,7 @@
 /*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:20:30 by sunhwang          #+#    #+#             */
-/*   Updated: 2023/05/29 19:55:59 by sunhwang         ###   ########.fr       */
+/*   Updated: 2023/05/29 22:05:09 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,7 @@ void ftSend(ResponseData *response, const std::string &response_header);
 bool isDirectory(const std::string &path);
 bool isFile(const std::string &path);
 bool isMethod(const std::string &method);
+bool writeFile(const std::string &path, const std::string &contents);
+std::string readFile(const std::string &path);
 
 #endif
