af5bb179d07b03d6e36160368439ccf06b916bac
get6 <ittae.com@gmail.com>
2023-05-30 22:22:58 +0900

    refactor: 미사용 함수, define 추가, 프로세스별 함수 분기 #8

--- src/cgi/CGI.hpp
+++ src/cgi/CGI.hpp
@@ -3,47 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   CGI.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yje <yje@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:35:20 by yje               #+#    #+#             */
-/*   Updated: 2023/05/17 16:29:28 by yje              ###   ########.fr       */
+/*   Updated: 2023/05/30 22:21:21 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 #define CGI_HPP
 
+#include <cctype>
 #include <iostream>
 #include <map>
-#include <unistd.h>
-#include <cctype>
 #include <sys/stat.h>
-#include "../parse/HTTPRequestParser.hpp"
-#include "../config/Config.hpp"
+#include <unistd.h>
+#include "Config.hpp"
+#include "HTTPRequestParser.hpp"
+
+#define R 0
+#define W 1
 
 class CGI
 {
 private:
 	void initEnvp(const HTTPRequest &request);
 	char **ENVPChangeStringArray();
-	std::string getBody() const;
-	void setBody(const std::string &body);
-	std::string getResponseBody() const;
-	void setEnv(const std::map<std::string, std::string> &env);
-	bool isCgiPath(void) const;
+	// std::string getBody() const;
+	// void setBody(const std::string &body);
+	// std::string getResponseBody() const;
+	// void setEnv(const std::map<std::string, std::string> &env);
+	void childProcess(const int fileFds[2], const std::string &program, char **envp);
+	void parentProcess(const pid_t &pid, const int fileFds[2], std::string &body);
+	// bool isCgiPath(void) const;
 
-	int fileFD_[2];
 	std::map<std::string, std::string> envp_;
-	std::string cgiPath_;
+	// std::string cgiPath_;
 	std::string body_;
-	//
+
 public:
-	CGI(const std::string &cgi_path);
 	CGI(const HTTPRequest &request);
-	CGI(const CGI &src);
 	~CGI(void);
 
-	std::string excuteCGI(const std::string &context, const HTTPRequest &request);
+	std::string excuteCGI(const std::string &context);
 	// void deleteEnvp(char** envp) const; -> 수정 예정
 };
 #endif
